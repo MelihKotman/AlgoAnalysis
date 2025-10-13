cea6030cc203b2b852e3e1f91becf4011e161558
Melih <melihkotman@gmail.com>
2025-10-13 17:30:04 +0300

    insertionSort.c added comment for explaination for code

--- AlgoAnalysis-C/InsertionSort/insertionSort.c
+++ AlgoAnalysis-C/InsertionSort/insertionSort.c
@@ -11,8 +11,7 @@
 
 void InsertionSort(int A[]);
 
-int main()
-{
+int main(){
     int numbers1[ARRAYSIZE];
     int i,j;
 
@@ -20,39 +19,42 @@ int main()
     time_t t; // zaman değişkeni oluştur
     srand((unsigned) time(&t)); // Rastgele sayı oluşturucuyu bilgisayarın saati ile eşle
 
-    for(i = 0; i < ARRAYSIZE; i++)
-    {
+    for(i = 0; i < ARRAYSIZE; i++){
         numbers1[i] = rand(); // 0 ile max int arası sayı oluştur.
     }
 
-    for(i = 0; i < ARRAYSIZE; i++)
+    for(i = 0; i < ARRAYSIZE; i++) {
         printf("%d:\t%d\n",i+1,numbers1[i]);
+    }
 
     clock_t start_t, end_t;
     double cpu_time_used;
     start_t = clock();
     InsertionSort(numbers1);
     end_t = clock();
+
     cpu_time_used = (double)(end_t - start_t) / CLOCKS_PER_SEC;
     printf("Toplam gecen sure (Eklemeli Siralama): %f saniyedir.\n", cpu_time_used);
 
     printf("********************SIRALAMA SONRASI****************************\n");
 
-   for(i = 0; i < ARRAYSIZE; i++)
-        printf("%d:\t%d\n",i+1,numbers1[i]);
+   for(i = 0; i < ARRAYSIZE; i++) {
+       printf("%d:\t%d\n",i+1,numbers1[i]);
+   }
     return 0;
 
 }
 
 // Algoritmanın içini doldurunuz
 void InsertionSort(int A[]){
-for (int j = 1; j < ARRAYSIZE; j++) {
-    int key = A[j];
-    int i = j - 1;
+for (int j = 1; j < ARRAYSIZE; j++) { //başlangıcın bir fazlasından sona kadar başla
+    int key = A[j];//Sonraki eleman hep bizim anahtarımız
+    int i = j - 1;//Anahtarın bir öncesini aldık
+    //Anahtar, solundaki dizide yer bulana kadar yer değişimi yap
     while (i >= 0 && A[i] > key) {
         A[i+1] = A[i];
         i = i - 1;
     }
-    A[i+1] = key;
+    A[i+1] = key;//Bir sonraki anahtara geç
 }
 }
